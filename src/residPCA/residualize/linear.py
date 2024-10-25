a168d6f44589311e3b8e5b99c86610a5485adaf5
Stefan Groha <s.groha@gmail.com>
2024-10-25 14:46:36 -0400

    rename and remove residual scaler (should be done with scanpy)

--- src/residPCA/residualize/linear.py
+++ src/residPCA/residualize/linear.py
@@ -5,7 +5,7 @@ import scipy.linalg
 from numpy import ndarray
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.base import TransformerMixin
 from sklearn.pipeline import Pipeline
 
 
@@ -32,7 +32,7 @@ def _test_onehot(C: ndarray) -> bool:
 
 def _linear_regression_effectsizes(
             X: ndarray,
-            C: ndarray,
+            y: ndarray,
             method: Literal['onehot', 'lstsq', 'pinv'] = 'lstsq'
         ) -> Union[ndarray, ndarray]:
     """
@@ -42,8 +42,8 @@ def _linear_regression_effectsizes(
     ----------
     X : np.ndarray
         Data matrix.
-    C : np.ndarray
-        Covariates matrix.
+    y: np.ndarray
+        Outcome matrix
     method : {'onehot', 'lstsq', 'pinv'}
         Method to use for the regression. 'onehot' can be used when the
         covariates are one-hot encoded and is more efficient for large number
@@ -58,53 +58,58 @@ def _linear_regression_effectsizes(
         Effect sizes from the regression.
     """
     if method == 'onehot':
-        if _test_onehot(C):
-            beta = (C.T @ X) / np.sum(C, axis=0)[:, None]
+        if _test_onehot(X):
+            beta = (X.T @ y) / np.sum(X, axis=0)[:, None]
         else:
             raise ValueError(
                 'Data has to be one-hot encoded to use the onehot method.')
     elif method == 'lstsq':
-        beta = scipy.linalg.lstsq(C, X)[0]
+        beta = scipy.linalg.lstsq(X, y)[0]
     elif method == 'pinv':
-        beta = np.linalg.pinv(C) @ X
+        beta = np.linalg.pinv(X) @ y
     else:
         raise ValueError(
             'Method not recognized. Use one of "onehot", "lstsq", or "pinv".')
     return beta
 
 
-class _LinearRegressionResiduals(BaseEstimator, TransformerMixin):
+class _LinearRegressionResiduals(TransformerMixin):
     """Linear regression without scaling option"""
 
     def __init__(
             self,
-            include_intercept: bool = False,
+            include_intercept: bool = True,
             method: Literal['onehot', 'lstsq', 'pinv'] = 'lstsq'
     ) -> None:
         self.include_intercept = include_intercept
         self.method = method
-        self.c_scaler = StandardScaler()
 
-    def fit(self, X: ndarray, C: ndarray) -> '_LinearRegressionResiduals':
-        if self.method != 'onehot':
-            C = self.c_scaler.fit_transform(C)
+    def fit(self, X: ndarray, y: ndarray) -> '_LinearRegressionResiduals':
         if self.include_intercept:
-            ones_column = np.ones((C.shape[0], 1))
-            C = np.hstack((ones_column, C))
-        self.beta = _linear_regression_effectsizes(X, C, self.method)
+            if self.method == 'onehot':
+                raise ValueError(
+                    'One-hot encoding is not supported with an intercept.'
+                    'Set intercept to False and standard_scale to True.'
+                )
+            ones_column = np.ones((X.shape[0], 1))
+            X = np.hstack((ones_column, X))
+        self.beta = _linear_regression_effectsizes(X, y, self.method)
         return self
 
-    def transform(self, X: ndarray, C: ndarray) -> ndarray:
-        if self.method != 'onehot':
-            C = self.c_scaler.transform(C)
+    def transform(self, X: ndarray, y: ndarray) -> ndarray:
         if self.include_intercept:
+            if self.method == 'onehot':
+                raise ValueError(
+                    'One-hot encoding is not supported with an intercept.'
+                    'Set intercept to False and standard_scale to True.'
+                )
             ones_column = np.ones((X.shape[0], 1))
-            C = np.hstack((ones_column, C))
-        residual = X - C @ self.beta
+            X = np.hstack((ones_column, X))
+        residual = y - X @ self.beta
         return residual
 
-    def fit_transform(self, X: ndarray, C: ndarray) -> ndarray:
-        return self.fit(X, C).transform(X, C)
+    def fit_transform(self, X: ndarray, y: ndarray) -> ndarray:
+        return self.fit(X, y).transform(X, y)
 
     def get_effectsizes(self) -> ndarray:
         return self.beta
@@ -116,25 +121,31 @@ class LinearRegressionResiduals(Pipeline):
 
     Parameters
     ----------
-    include_intercept : bool
-        Whether to include an intercept in the regression. Not needed if the
-        data matrix is already centered.
-    standard_scale : bool
-        Whether to standard scale the residuals before returning them.
+    standard_scale_covars : bool, default=True
+        Whether to standard scale the covariates matrix.
+    include_intercept : bool, default=True
+        Whether to include an intercept in the regression. Not needed if
+        the data matrix is already centered and standard_scale_covars is
+        False.
     method : {'onehot', 'lstsq', 'pinv'}
-        Method to use for the regression. 'onehot' can be used when the
-        covariates are one-hot encoded and is more efficient for large number
-        of cells, 'lstsq' is used for the least squares solution (stable), and
-        'pinv' is used for the pseudo-inverse solution (faster than 'lstsq' for
-        large number of cells, but can be unstable for ill-conditioned
-        matrices).
+        Method to use for the regression.
+            - 'lstsq' is used for the least squares solution. This method is
+            stable with respect to ill-conditioned covariate matrices, but
+            can be slow for large number of cells.
+            - 'onehot' can be used when the covariates are one-hot encoded
+            and is more efficient for large number of cells. The data
+            matrix should be standardized before using this method, the
+            options standard_scale_covars and include_intercept should be
+            set to False.
+            - 'pinv' is used for the pseudo-inverse solution. This method is
+            faster than 'lstsq' for large number of cells, but can be
+            unstable for ill-conditioned matrices.
     """
 
     def __init__(
             self,
-            standard_scale: bool = True,
-            standard_scale_residuals: bool = True,
-            include_intercept: bool = False,
+            standard_scale_covars: bool = True,
+            include_intercept: bool = True,
             method: Literal['onehot', 'lstsq', 'pinv'] = 'lstsq'
     ) -> None:
         """
@@ -142,32 +153,39 @@ class LinearRegressionResiduals(Pipeline):
 
         Parameters
         ----------
-        standard_scale : bool
-            Whether to standard scale the residuals before returning them.
-        include_intercept : bool
+        standard_scale_covars : bool, default=True
+            Whether to standard scale the covariates matrix.
+        include_intercept : bool, default=True
             Whether to include an intercept in the regression. Not needed if
-            the data matrix is already centered.
-        standard_scale_residuals : bool
-            Whether to standard scale the residuals before returning them.
+            the data matrix is already centered and standard_scale_covars is
+            False.
         method : {'onehot', 'lstsq', 'pinv'}
-            Method to use for the regression. 'onehot' can be used when the
-            covariates are one-hot encoded and is more efficient for large
-            number of cells, 'lstsq' is used for the least squares solution
-            (stable), and 'pinv' is used for the pseudo-inverse solution
-            (faster than 'lstsq' for large number of cells, but can be unstable
-            for ill-conditioned matrices).
+            Method to use for the regression.
+             - 'lstsq' is used for the least squares solution. This method is
+                stable with respect to ill-conditioned covariate matrices, but
+                can be slow for large number of cells.
+             - 'onehot' can be used when the covariates are one-hot encoded
+                and is more efficient for large number of cells. The data
+                matrix should be standardized before using this method, the
+                options standard_scale_covars and include_intercept should be
+                set to False.
+              - 'pinv' is used for the pseudo-inverse solution. This method is
+                faster than 'lstsq' for large number of cells, but can be
+                unstable for ill-conditioned matrices.
         """
         steps = [
             ("linreg", _LinearRegressionResiduals(
                 include_intercept=include_intercept, method=method)),
         ]
-        if standard_scale:
-            steps.insert(0, ["scaler", StandardScaler()])
-        if standard_scale_residuals:
-            steps.append(["residual scaler", StandardScaler()])
+        if standard_scale_covars:
+            if method == 'onehot':
+                raise ValueError(
+                    'One-hot encoding is not supported with standard scaling.'
+                    'Set standard_scale_covars to False.'
+                )
+            steps.insert(0, ("covar scaler", StandardScaler()))
         super().__init__(steps)
-        self.standard_scale = standard_scale
-        self.standard_scale_residuals = standard_scale_residuals
+        self.standard_scale_covars = standard_scale_covars
         self.include_intercept = include_intercept
         self.method = method
 
