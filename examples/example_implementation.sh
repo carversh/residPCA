02d4b9c471e1100929e4c0fa7da9aaac4da08607
Shaye Carver <scarver@g.harvard.edu>
2024-12-15 19:18:32 -0500

    updating with heritability commands

--- examples/example_implementation.sh
+++ examples/example_implementation.sh
@@ -27,3 +27,9 @@ ResidPCA residPCA_fit --basename test_run --path_to_directory ./
 ResidPCA Iter_PCA_fit --basename test_run --path_to_directory ./
 
 ResidPCA ID_Global_CellType_States --basename test_run --path_to_directory ./
+
+ResidPCA heritability_bed_output --basename test_run --path_to_directory ./ --ref_annotations_file ~/residPCA/gencode.v39.basic.annotation.names.bed --method Resid
+
+ResidPCA heritability_bed_output --basename test_run --path_to_directory ./ --ref_annotations_file ~/residPCA/gencode.v39.basic.annotation.names.bed --method Standard
+
+ResidPCA heritability_bed_output --basename test_run --path_to_directory ./ --ref_annotations_file ~/residPCA/gencode.v39.basic.annotation.names.bed --method Iter
